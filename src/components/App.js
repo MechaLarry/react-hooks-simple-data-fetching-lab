3aebaac4740c37f8243f25c2d171ae515b3387d2
MechaLarry <mecha.larry@gmail.com>
2024-08-13 22:35:20 +0300

    My first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,35 @@
 // create your App component here
+import React, {useState, useEffect} from "react";
+
+function App() {
+ // define the variables in use   
+ const [imageUrl, setImageUrl] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+
+useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+    .then((response)=> response.json())
+    .then((data)=> {
+        setImageUrl(data.message);
+        setIsLoading(false);
+    })
+    .catch((error)=> {
+        SVGComponentTransferFunctionElement.error("error fetching the image:", error);
+        setIsLoading(false);
+    });
+    
+}, []);
+
+return (
+  <div>
+    {isLoading ? (
+      <p>Loading...</p>
+    ) : (
+      <img src={imageUrl} alt="A Random Dog" />
+    )}
+  </div>
+);
+}
+
+export default App;
